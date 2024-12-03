a6f37db16965b4e178de07ce5473ca2e574a79a7
Caleb <caleb.a.collar@gmail.com>
2024-12-03 00:14:57 -0600

    feat: day three part one solution

--- solutions/3/day3.ts
+++ solutions/3/day3.ts
@@ -1,10 +1,24 @@
-import { ArrExt, colors, reset } from '../../util/util.ts';
+import { colors, reset } from '../../util/util.ts';
 
 const dayThree = (lines: string[]) => {
   console.log(`${colors[3]}Mull It Over 🧮${reset}`);
-  //TODO: Implement day 3 solution
-  console.log('❄ INSTRUCTION RESULTS: ');
-  console.log('❄ : ');
+
+  const total = getTotal(getMulMatches(lines.join()));
+
+  console.log('❄ INSTRUCTION RESULTS: ', total);
+  console.log('❄ CONDITIONAL INSTRUCTION RESULTS: ');
+};
+
+const getMulMatches = (input: string) => {
+  const mulRegex = /mul\((\d+),(\d+)\)/g;
+  return input.matchAll(mulRegex);
+};
+
+const getTotal = (matches: RegExpStringIterator<RegExpExecArray>) => {
+  return matches.reduce(
+    (acc, match) => acc + (Number(match[1]) * Number(match[2])),
+    0,
+  );
 };
 
 export default dayThree;
