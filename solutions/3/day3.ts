cacdc6dae88d33078b691f4ab1dd29031852a155
Caleb <caleb.a.collar@gmail.com>
2024-12-02 23:45:26 -0600

    feat: set up day three input and solution

---
+++ solutions/3/day3.ts
@@ -0,0 +1,10 @@
+import { ArrExt, colors, reset } from '../../util/util.ts';
+
+const dayThree = (lines: string[]) => {
+  console.log(`${colors[3]}Mull It Over 🧮${reset}`);
+  //TODO: Implement day 3 solution
+  console.log('❄ INSTRUCTION RESULTS: ');
+  console.log('❄ : ');
+};
+
+export default dayThree;
