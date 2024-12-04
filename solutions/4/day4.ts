741d6d72efa6a85304edd7c5918721e2a1878cc5
Caleb <caleb.a.collar@gmail.com>
2024-12-04 16:59:17 -0600

    feat: start of day four

---
+++ solutions/4/day4.ts
@@ -0,0 +1,14 @@
+import { colors, reset } from '../../util/util.ts';
+
+const title = 'Ceres Search 🛰️';
+
+const dayFour = (lines: string[]) => {
+  console.log(`${colors[3]}${title}${reset}`);
+  //Invoke solution
+  //I'm going to use a graph and search nodes starting from every X node
+
+  console.log('❄ WORD SEARCH RESULTS: ');
+  console.log('❄ ');
+};
+
+export default dayFour;
