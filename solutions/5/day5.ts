ec74670dc0f45a7b761b0aa20c055b79d45ecf9e
Caleb <caleb.a.collar@gmail.com>
2024-12-06 13:00:26 -0600

    feat: complete day 5 part 1

--- solutions/5/day5.ts
+++ solutions/5/day5.ts
@@ -5,20 +5,37 @@ const title = 'Print Queue 🖨️';
 const dayFive = (lines: string[]) => {
   console.log(`${bold}${colors[3]}${title}${reset}`);
 
-  console.log(getRulesAndPages(lines));
+  const validRulesAndUpdates = checkUpdatesValid(getRulesAndUpdates(lines));
+  const updateMidSum = sumValidMiddleValues(validRulesAndUpdates);
 
-  console.log('❄ PAGE ORDER SUM: ');
-  console.log('❄ : ');
+  console.log('❄ VALID UPDATES MIDDLE SUM: ', updateMidSum);
+  console.log('❄ P2: ');
 };
 
-const getRulesAndPages = (
+const getRulesAndUpdates = (
   lines: string[],
-): { rules: string[]; pages: string[] } => {
+) => {
   const splitIndex = lines.findIndex((line) => line === '');
   return {
-    rules: lines.slice(0, splitIndex),
-    pages: lines.slice(splitIndex + 1).flatMap((p) => p.split(',')),
+    rules: lines.slice(0, splitIndex).map((r) => r.split('|').map(Number)),
+    updates: lines.slice(splitIndex + 1).map((u) => u.split(',').map(Number)),
   };
 };
+
+const checkUpdatesValid = ({
+  rules,
+  updates,
+}: ReturnType<typeof getRulesAndUpdates>) =>
+  updates.map((update) => ({
+    valid: rules
+      .filter(([a, b]) => update.includes(a) && update.includes(b))
+      .every(([a, b]) => update.indexOf(a) < update.indexOf(b)),
+    update,
+  }));
+
+const sumValidMiddleValues = (entries: ReturnType<typeof checkUpdatesValid>) =>
+  entries
+    .filter(({ valid }) => valid)
+    .reduce((sum, { update }) => sum + update[~~(update.length / 2)], 0);
 
 export default dayFive;
