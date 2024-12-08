6f3f2da55be080119691ff78e72f8571a150f55e
Caleb <caleb.a.collar@gmail.com>
2024-12-08 14:45:08 -0600

    feat: day 8 template

---
+++ solutions/8/day8.ts
@@ -0,0 +1,12 @@
+import { bold, colors, reset } from '../../util/util.ts';
+
+const title = 'Resonant Collinearity 📡';
+
+const dayEight = (lines: string[]) => {
+  console.log(`${bold}${colors[3]}${title}${reset}`);
+
+  console.log('❄ pt1: ');
+  console.log('❄ pt2: ');
+};
+
+export default dayEight;
