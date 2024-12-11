2ecb5dd2ddde291930d8b9abe83aab0f57d981ed
Caleb <caleb.a.collar@gmail.com>
2024-12-11 17:39:24 -0600

    fix: spelling

--- solutions/9/day9.ts
+++ solutions/9/day9.ts
@@ -2,11 +2,11 @@ import { bold, colors, reset } from '../../util/util.ts';
 
 const title = 'Disk Fragmenter 📀';
 
-const dayX = (lines: string[]) => {
+const dayNine = (lines: string[]) => {
   console.log(`${bold}${colors[3]}${title}${reset}`);
 
   console.log('❄ pt1: ');
   console.log('❄ pt2: ');
 };
 
-export default dayX;
+export default dayNine;
