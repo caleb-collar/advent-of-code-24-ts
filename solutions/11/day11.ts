8ba7075d300b122b91a35bf8399dd6508074bc1d
Caleb <caleb.a.collar@gmail.com>
2024-12-12 13:30:32 -0600

    feat: template for day 11

---
+++ solutions/11/day11.ts
@@ -0,0 +1,12 @@
+import { bold, colors, reset } from '../../util/util.ts';
+
+const title = 'Plutonian Pebbles ♇';
+
+const dayEleven = (lines: string[]) => {
+  console.log(`${bold}${colors[3]}${title}${reset}`);
+
+  console.log('❄ pt1: ');
+  console.log('❄ pt2: ');
+};
+
+export default dayEleven;
