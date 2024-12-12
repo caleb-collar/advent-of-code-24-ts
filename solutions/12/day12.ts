c4f1e8546029409338daa0d3c3f0473c3169d4b8
Caleb <caleb.a.collar@gmail.com>
2024-12-12 13:30:42 -0600

    feat: template for day 12

---
+++ solutions/12/day12.ts
@@ -0,0 +1,12 @@
+import { bold, colors, reset } from '../../util/util.ts';
+
+const title = 'Garden Groups 🌻';
+
+const dayTwelve = (lines: string[]) => {
+  console.log(`${bold}${colors[3]}${title}${reset}`);
+
+  console.log('❄ pt1: ');
+  console.log('❄ pt2: ');
+};
+
+export default dayTwelve;
